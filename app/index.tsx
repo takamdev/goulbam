ce37b25b04c8b15f9db7009b4ba00f6e4c94b9b5
Loic <panguslariss@gmail.com>
2025-02-07 18:34:34 +0100

    auth finish

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 
 export default function HomeScreen() {
    return (
@@ -7,11 +7,11 @@ export default function HomeScreen() {
          className="w-full h-screen top-0"
          source={require("@/assets/images/welcome.jpg")}
       >
-        {"img backgroung for welcome view"}
+        {/*"img backgroung for welcome view"*/}
 
          <View className="flex mt-28 relative h-3/4  items-center">
-            <Text className="text-3xl top-0 z-50 mt-32 font-bold  text-green-600">
-                Bienvenue au restaurand {'\n'}
+            <Text className="text-3xl top-0 z-50 mt-32 font-bold  text-green-500">
+                Bienvenue au restaurant {'\n'}
                <Text className="text-center">GoulBam</Text>
             </Text>
             <Text className="text-lg top-0 text-justify px-3 z-50 my-5  font-bold  text-white">
@@ -20,8 +20,8 @@ export default function HomeScreen() {
                porro hic, voluptate vitae quod placeat incidunt minima dicta
                laborum quia id!
             </Text>
-            {"go to login if not authantificate or Food authantificated"}
-            <Link className="text-white bg-red-600 p-5 rounded-md top-0 z-50" href="/login">
+            {/*"go to login if not authantificate or Food authantificated"*/}
+            <Link className=" text-2xl text-zinc-100 font-semibold bg-red-600 p-5 rounded-md top-0 z-50" href="/login">
                  passer une commande
             </Link>
          </View>
--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,14 +1,72 @@
-import { Link } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
+import InputLabel from "@/components/InputLabel";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { View, Text, ImageBackground, Button } from "react-native";
+import { useRouter } from 'expo-router';
+
 
 export default function HomeScreen() {
-  return (
-    <View className='flex justify-center items-center'>
-      <Text className='text-3xl text-green-600'>Login</Text>
+  const router = useRouter();
+  // init stat for the sign form
+  const [email,setEmail] = useState<string>("")
+  const [passWord,setPassword]=useState<string>("")
+  
+  // message for invalad email
+  const [message,setMessage] = useState(false)
+  //regex for email
+  const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  // init state for the validation form
+  const [valid,setValid]=useState([false,false])
+
+  // validation and connexion
+const sign = ()=>{
+  // verifation the empty fields
+  setValid([email?.trim().length === 0, passWord?.trim().length === 0]);
+  if (email?.trim().length === 0 || passWord?.trim().length === 0) return;
+  
+  // validation email
+  const isvalid = email_regex.test(email)
+  if(!isvalid) setMessage(true)
 
 
-      <Link href="/">Home</Link>
-    </View>
-  );
+    router.replace("/home")
+  
 }
 
+
+   return (
+      <ImageBackground
+         className="w-full h-screen top-0"
+         source={require("@/assets/images/welcome.jpg")}
+      >
+        {/*"img backgroung for login view"*/}
+
+         <View className="flex mt-16 relative h-3/4  items-center">
+            <Text className="text-3xl top-0 z-50 mt-32 font-bold  text-green-500">
+               Connectez-vous
+            </Text>
+            {/*"login View"*/}
+
+            <View style={{backgroundColor:"white",width:"95%"}} className={`flex  rounded-md  flex-col justify-center items-center my-5  z-50 h-1/2`}>
+               <InputLabel isPassword={false} value={email} onChange={(text) => setEmail(text)} label="Email ou Nom d'utilisateur"/>
+               {
+                message&& <Text className="text-red-500 self-start ms-4">email invalide</Text>
+               }
+               {
+                valid[0]&& <Text className="text-red-500 self-start ms-4">ce champ est requis</Text>
+               }
+
+               <InputLabel isPassword={true} value={passWord} onChange={(text) => setPassword(text)}  label="Mot de passe"/>
+               {
+                valid[1]&& <Text className="text-red-500 self-start ms-4">ce champ est requis</Text>
+               }
+            </View>
+            {/*"go to login if not authantificate or Food authantificated"*/}
+            <Text onPress={sign} role="button" className="text-white text-2xl  w-1/2 text-center  bg-red-600 p-5 rounded-md top-0 z-50" >
+                 Connexion
+            </Text>
+         </View>
+      </ImageBackground>
+   );
+}
